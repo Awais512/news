c1877b3c545350a04727a08deb52a5ba0f52c8e6
awais512 <raz.awais25@gmail.com>
2019-12-18 16:51:44 +0500

    Fixing minor header issues

--- resources/views/admin/author/edit.blade.php
+++ resources/views/admin/author/edit.blade.php
@@ -80,7 +80,6 @@
 </div>
 
 <script src="{{asset('admin/assets/js/vendor/jquery-2.1.4.min.js')}}"></script>
-<script src="{{asset('admin/assets/js/popper.min.js')}}"></script>
 <script src="{{asset('admin/assets/js/plugins.js')}}"></script>
 <script src="{{asset('admin/assets/js/main.js')}}"></script>
 <script src="{{asset('admin/assets/js/lib/chosen/chosen.jquery.min.js')}}"></script>
--- resources/views/admin/roles/list.blade.php
+++ resources/views/admin/roles/list.blade.php
@@ -105,7 +105,6 @@
     </div><!-- .content -->
 
     <script src="{{asset('admin/assets/js/vendor/jquery-2.1.4.min.js')}}"></script>
-    <script src="{{asset('admin/assets/js/popper.min.js')}}"></script>
     <script src="{{asset('admin/assets/js/plugins.js')}}"></script>
     <script src="{{asset('admin/assets/js/main.js')}}"></script>
 
